a670f5903b8ed5daf520dd8738760fc6bb5f72fc
KinGunyoung <KinGunyoung2821>
2022-11-29 19:42:29 +0900

    슬라이드 버튼

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,33 @@
-let slide = document.querySelector(`#slide`)
-let sliding = document.querySelectorAll(`#slide li`)
+const slide = document.querySelector(`#slide`)
+const sliding = document.querySelectorAll(`#slide li`)
+const prevBtn = document.querySelector(`.preview`)
+const nextBtn = document.querySelector(`.next`)
 
+let index = 0
+let interval = setInterval(slides, 1000)
 
-let index=0
-let interval = setInterval(slides , 1000)
+function slides(n) {
+    if (index === 3) index = 0
 
-function slides (n){
-    if (index ===3) index = 0
-
-    slide.setAttribute(`class`,`on`+(index+1))
+    slide.setAttribute(`class`, `on` + (index + 1))
     index++
 }
 console.log(interval)
+
+function prevBtnhandler() {
+    if(0 <index < 4) {
+    slide.setAttribute(`class`, `on` + (index - 1))
+    clearInterval(interval)
+    }
+}
+
+prevBtn.addEventListener(`click`, prevBtnhandler)
+
+function nextBtnhandler() {
+    if(0 < index < 4) {
+    slide.setAttribute(`class`, `on` + (index + 1))
+    clearInterval(interval)
+    }
+}
+
+nextBtn.addEventListener(`click`, nextBtnhandler)
