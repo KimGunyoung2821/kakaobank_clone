0e38e7d3bfaa434f576818aab4f16d891c4520ff
KinGunyoung <KinGunyoung2821>
2022-12-03 20:33:36 +0900

    페이저 버튼 완료

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,13 @@
 const slide = document.querySelector(`.slide`) //ul
 const img123 = document.querySelectorAll(`.img123`) //li
-
+const checkcard2 = document.querySelector(`#checkcard2`)
 const prevbtn = document.querySelector(`.preview`)
 const nextbtn = document.querySelector(`.next`)
+const pagerBtn = document.querySelectorAll(`.pager span`)
 
 const img123Count = img123.length
 let currentIndex = 0
+
 
 console.log(img123.length)
 for (let i = 0; i < img123.length; i++) {
@@ -15,19 +17,25 @@ for (let i = 0; i < img123.length; i++) {
 function gotoslide(idx) {
     slide.style.left = -100 * idx + `%`;
     currentIndex = idx
+
+    for (let y = 0; y < pagerBtn.length; y++) {
+        pagerBtn[y].classList.remove(`active`)
+    }
+    pagerBtn[idx].classList.add(`active`)
 }
+
+gotoslide(0)
 
 prevbtn.addEventListener(`click`, function () {
     // currentIndex -= 1
     // gotoslide(currentIndex)
 
-    if(currentIndex == 0) {
-        gotoslide(img123Count -1)
-    } else{
+    if (currentIndex == 0) {
+        gotoslide(img123Count - 1)
+    } else {
         currentIndex -= 1
         gotoslide(currentIndex)
     }
-
 
 })
 
@@ -39,6 +47,38 @@ nextbtn.addEventListener(`click`, function () {
         gotoslide(0)
     } else {
         currentIndex += 1
-        gotoslide(currentIndex )
+        gotoslide(currentIndex)
     }
 })
+
+let interval = setInterval(function () {
+    const nextIndex = (currentIndex + 1) % img123Count
+
+    gotoslide(nextIndex)
+}, 3000)
+
+
+
+checkcard2.addEventListener(`mouseover`, function () {
+    clearInterval(interval)
+})
+
+checkcard2.addEventListener(`mouseout`, function () {
+    interval = setInterval(function () {
+        const nextIndex = (currentIndex + 1) % img123Count
+
+        gotoslide(nextIndex)
+    }, 3000)
+})
+
+for (let i = 0; i < pagerBtn.length; i++) {
+    pagerBtn[i].addEventListener(`click`, function (e) {
+        console.log(e.target.innerText)
+        // let pagerNum = e.target.getAttribute(`data-idx`)
+        let pagerNum = e.target.innerText - 1
+        gotoslide(pagerNum)
+
+        
+    })
+
+}
